f31c3a91db316157af9849b78d428fe2098f3d85
b333vv <b333vv@gmail.com>
2020-01-31 08:56:51 +0300

    Cleanup

--- src/main/java/org/jacoquev/model/metric/Sets.java
+++ src/main/java/org/jacoquev/model/metric/Sets.java
@@ -1,4 +1,47 @@
 package org.jacoquev.model.metric;
 
+import java.util.Set;
+
 public class Sets {
+//    Chidamber-Kemerer metrics set includes:
+//    WMC: Weighted methods per class
+//    DIT: Depth of Inheritance Tree
+//    NOC: Number of Children
+//    CBO: Coupling between object classes
+//    RFC: Response for a Class
+//    LCOM: Lack of cohesion in methods
+    private Set<String> chidamberKemererMetricSet = Set.of("WMC", "DIT", "NOC", "CBO", "RFC", "LCOM");
+
+//    Robert C. Martin metrics set includes:
+//    Ce: Efferent Coupling
+//    Ca: Afferent Coupling
+//    I: Instability
+//    A: Abstractness
+//    D: Normalized Distance from Main Sequence
+    private Set<String> robertMartinMetricSet = Set.of("Ce", "Ca", "I", "A", "D");
+
+//    MOOD metrics set includes:
+//    MHF: Method Hiding Factor
+//    AHF: Attribute Hiding Factor
+//    MIF: Method Inheritance Factor
+//    AIF: Attribute Inheritance Factor
+//    PF: Polymorphism Factor
+//    CF: Coupling Factor
+    private Set<String> moodMetricSet = Set.of("MHF", "AHF", "MIF", "AIF", "PF", "CF");
+
+//    Lorenz-Kidd metrics set includes:
+//    NOA: Number of Attributes
+//    NOO: Number of Operations
+//    NOAM: Number of Added Methods
+//    NOOM: Number of Overridden Methods
+    private Set<String> lorenzKiddMetricSet = Set.of("NOA", "NOO", "NOAM", "NOOM");
+
+//    Wei Li metrics set includes:
+//    NAC: Number of Ancestor Classes
+//    NLM: Number of Local Methods
+//    CMC: Class Method Complexity
+//    NDC: Number of Descendent Classes
+//    CTA: Coupling Through Abstract Data Type
+//    CTM: Coupling Through Message Passing
+    private Set<String> weiLiMetricSet = Set.of("NAC", "NLM", "CMC", "NDC", "CTA", "CTM");
 }
--- src/main/java/org/jacoquev/model/metric/meter/type/WeightedMethodCount.java
+++ src/main/java/org/jacoquev/model/metric/meter/type/WeightedMethodCount.java
@@ -1,14 +1,11 @@
 package org.jacoquev.model.metric.meter.type;
 
 import com.google.common.collect.ImmutableSet;
-import com.intellij.psi.PsiAnonymousClass;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiDeclarationStatement;
-import com.intellij.psi.PsiTypeParameter;
+import com.intellij.psi.*;
+import com.intellij.psi.tree.IElementType;
 import org.jacoquev.model.code.JavaClass;
 import org.jacoquev.model.metric.Meter;
 import org.jacoquev.model.metric.Metric;
-import org.jacoquev.model.metric.meter.CyclomaticComplexity;
 import org.jacoquev.util.MetricsUtils;
 
 import java.util.Arrays;
@@ -37,7 +34,100 @@ public class WeightedMethodCount implements Meter<JavaClass> {
 
     private long getWeightedMethodCount(PsiClass psiClass) {
         return Arrays.stream(psiClass.getMethods())
-                .mapToLong(CyclomaticComplexity::calculate)
+                .mapToLong(this::calculateMethodComplexity)
                 .sum();
+    }
+
+    public long calculateMethodComplexity(PsiMethod psiMethod) {
+        if (psiMethod == null) {
+            return 1;
+        }
+        MethodComplexityVisitor visitor = new MethodComplexityVisitor();
+        psiMethod.accept(visitor);
+        return visitor.getMethodComplexity();
+    }
+
+    private static class MethodComplexityVisitor extends JavaRecursiveElementWalkingVisitor {
+
+        private long methodComplexity = 1;
+
+        @Override
+        public void visitForStatement(PsiForStatement statement) {
+            super.visitForStatement(statement);
+            methodComplexity++;
+        }
+
+        @Override
+        public void visitForeachStatement(PsiForeachStatement statement) {
+            super.visitForeachStatement(statement);
+            methodComplexity++;
+        }
+
+        @Override
+        public void visitIfStatement(PsiIfStatement statement) {
+            super.visitIfStatement(statement);
+            methodComplexity++;
+        }
+
+        @Override
+        public void visitDoWhileStatement(PsiDoWhileStatement statement) {
+            super.visitDoWhileStatement(statement);
+            methodComplexity++;
+        }
+
+        @Override
+        public void visitConditionalExpression(PsiConditionalExpression expression) {
+            super.visitConditionalExpression(expression);
+            methodComplexity++;
+        }
+
+        @Override
+        public void visitSwitchStatement(PsiSwitchStatement statement) {
+            super.visitSwitchStatement(statement);
+            final PsiCodeBlock body = statement.getBody();
+            if (body == null) {
+                return;
+            }
+            final PsiStatement[] statements = body.getStatements();
+            boolean pendingLabel = false;
+            boolean accepted = true;
+            for (final PsiStatement child : statements) {
+                if (child instanceof PsiSwitchLabelStatement) {
+                    if (!pendingLabel && accepted) {
+                        methodComplexity++;
+                    }
+                    accepted = true;
+                    pendingLabel = true;
+                } else {
+                    accepted = true;
+                    pendingLabel = false;
+                }
+            }
+        }
+
+        @Override
+        public void visitWhileStatement(PsiWhileStatement statement) {
+            super.visitWhileStatement(statement);
+            methodComplexity++;
+        }
+
+        @Override
+        public void visitCatchSection(PsiCatchSection section) {
+            super.visitCatchSection(section);
+            methodComplexity++;
+        }
+
+        @Override
+        public void visitPolyadicExpression(PsiPolyadicExpression expression) {
+            super.visitPolyadicExpression(expression);
+            final IElementType token = expression.getOperationTokenType();
+            if (token.equals(JavaTokenType.ANDAND) || token.equals(JavaTokenType.OROR)) {
+                methodComplexity += expression.getOperands().length - 1;
+            }
+        }
+
+        public long getMethodComplexity() {
+            return methodComplexity;
+        }
     }
 }
