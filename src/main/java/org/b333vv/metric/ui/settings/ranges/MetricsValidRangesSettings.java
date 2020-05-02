7bd3c3de076c0d44213297555793879eeaed0b9d
b333vv <b333vv@gmail.com>
2020-05-02 15:59:45 +0300

    Cleanup

--- src/main/java/org/b333vv/metric/ui/settings/ranges/MetricsValidRangesSettings.java
+++ src/main/java/org/b333vv/metric/ui/settings/ranges/MetricsValidRangesSettings.java
@@ -52,84 +52,77 @@ public final class MetricsValidRangesSettings implements PersistentStateComponen
         controlValidRanges = true;
 
         //Chidamber-Kemerer metrics set
-        controlledMetrics.put(WMC.name(), new MetricsValidRangeStub(WMC.name(), WMC.description(), WMC.level().level(),
-                false, 0.00, 0.00, 0, 24));
-        controlledMetrics.put(DIT.name(), new MetricsValidRangeStub(DIT.name(), DIT.description(), DIT.level().level(),
-                false, 0.00, 0.00, 0, 5));
-        controlledMetrics.put(CBO.name(), new MetricsValidRangeStub(CBO.name(), CBO.description(), CBO.level().level(),
-                false, 0.00, 0.00, 0, 13));
-        controlledMetrics.put(RFC.name(), new MetricsValidRangeStub(RFC.name(), RFC.description(), RFC.level().level(),
-                false, 0.00, 0.00, 0, 44));
-
+        putToControlledMetricsMap(WMC, 0, 24);
+        putToControlledMetricsMap(DIT, 0, 5);
+        putToControlledMetricsMap(CBO, 0, 13);
+        putToControlledMetricsMap(RFC, 0, 44);
 
         //Lorenz-Kidd metrics set
-        controlledMetrics.put(NOA.name(), new MetricsValidRangeStub(NOA.name(), NOA.description(), NOA.level().level(),
-                false, 0.00, 0.00, 0, 40));
-        controlledMetrics.put(NOO.name(), new MetricsValidRangeStub(NOO.name(), NOO.description(), NOO.level().level(),
-                false, 0.00, 0.00, 0, 20));
-        controlledMetrics.put(NOOM.name(), new MetricsValidRangeStub(NOOM.name(), NOOM.description(), NOOM.level().level(),
-                false, 0.00, 0.00, 0, 3));
+        putToControlledMetricsMap(NOA, 0, 40);
+        putToControlledMetricsMap(NOO, 0, 20);
+        putToControlledMetricsMap(NOOM, 0, 3);
 
         //Robert C. Martin metrics set
-        controlledMetrics.put(Ce.name(), new MetricsValidRangeStub(Ce.name(), Ce.description(), Ce.level().level(),
-                false, 0.00, 0.00, 0, 20));
-        controlledMetrics.put(Ca.name(), new MetricsValidRangeStub(Ca.name(), Ca.description(), Ca.level().level(),
-                false, 0.00, 0.00, 0, 500));
-        controlledMetrics.put(I.name(), new MetricsValidRangeStub(I.name(), I.description(), I.level().level(),
-                true, 0.00, 1.00, 0, 0));
-        controlledMetrics.put(A.name(), new MetricsValidRangeStub(A.name(), A.description(), A.level().level(),
-                true, 0.00, 1.00, 0, 0));
-        controlledMetrics.put(D.name(), new MetricsValidRangeStub(D.name(), D.description(), D.level().level(),
-                true, 0.00, 0.70, 0, 0));
+        putToControlledMetricsMap(Ce, 0, 20);
+        putToControlledMetricsMap(Ca, 0, 500);
+        putToControlledMetricsMap(I, 0.00, 1.00);
+        putToControlledMetricsMap(A, 0.00, 1.00);
+        putToControlledMetricsMap(D, 0.00, 0.70);
 
         //MOOD metrics set
-        controlledMetrics.put(MHF.name(), new MetricsValidRangeStub(MHF.name(), MHF.description(), MHF.level().level(),
-                true, 0.095, 0.369, 0, 0));
-        controlledMetrics.put(AHF.name(), new MetricsValidRangeStub(AHF.name(), AHF.description(), AHF.level().level(),
-                true, 0.677, 1.0, 0, 0));
-        controlledMetrics.put(MIF.name(), new MetricsValidRangeStub(MIF.name(), MIF.description(), MIF.level().level(),
-                true, 0.609, 0.844, 0, 0));
-        controlledMetrics.put(AIF.name(), new MetricsValidRangeStub(AIF.name(), AIF.description(), AIF.level().level(),
-                true, 0.374, 0.757, 0, 0));
-        controlledMetrics.put(CF.name(), new MetricsValidRangeStub(CF.name(), CF.description(), CF.level().level(),
-                true, 0.00, 0.243, 0, 0));
-        controlledMetrics.put(PF.name(), new MetricsValidRangeStub(PF.name(), PF.description(), PF.level().level(),
-                true, 0.017, 0.151, 0, 0));
+        putToControlledMetricsMap(MHF, 0.095, 0.369);
+        putToControlledMetricsMap(AHF, 0.677, 1.00);
+        putToControlledMetricsMap(MIF, 0.609, 0.844);
+        putToControlledMetricsMap(AIF, 0.374, 0.757);
+        putToControlledMetricsMap(CF, 0.00, 0.243);
+        putToControlledMetricsMap(PF, 0.017, 0.151);
 
         //Methods metrics set
-        controlledMetrics.put(LOC.name(), new MetricsValidRangeStub(LOC.name(), LOC.description(), LOC.level().level(),
-                false, 0.00, 0.00, 0, 500));
+        putToControlledMetricsMap(LOC, 0, 500);
 
-
+        //-----------------------------
         //Chidamber-Kemerer metrics set
-        unControlledMetrics.put(NOC.name(), new MetricsValidRangeStub(NOC.name(), NOC.description(), NOC.level().level(),
-                false, 0.00, 0.00, 0, 100));
-        unControlledMetrics.put(LCOM.name(), new MetricsValidRangeStub(LCOM.name(), LCOM.description(), LCOM.level().level(),
-                false, 0.00, 0.00, 0, 500));
+        putToUnControlledMetricsMap(NOC, 0, 100);
+        putToUnControlledMetricsMap(LCOM, 0, 500);
 
         //Lorenz-Kidd metrics set
-        unControlledMetrics.put(NOAM.name(), new MetricsValidRangeStub(NOAM.name(), NOAM.description(), NOAM.level().level(),
-                false, 0.00, 0.00, 0, 10));
+        putToUnControlledMetricsMap(NOAM, 0, 10);
 
         //Li-Henry metrics set
-        unControlledMetrics.put(SIZE2.name(), new MetricsValidRangeStub(SIZE2.name(), SIZE2.description(), SIZE2.level().level(),
-                false, 0.00, 0.00, 0, 130));
-        unControlledMetrics.put(NOM.name(), new MetricsValidRangeStub(NOM.name(), NOM.description(), NOM.level().level(),
-                false, 0.00, 0.00, 0, 25));
-        unControlledMetrics.put(MPC.name(), new MetricsValidRangeStub(MPC.name(), MPC.description(), MPC.level().level(),
-                false, 0.00, 0.00, 0, 10));
-        unControlledMetrics.put(DAC.name(), new MetricsValidRangeStub(DAC.name(), DAC.description(), DAC.level().level(),
-                false, 0.00, 0.00, 0, 15));
+        putToUnControlledMetricsMap(SIZE2, 0, 130);
+        putToUnControlledMetricsMap(NOM, 0, 25);
+        putToUnControlledMetricsMap(MPC, 0, 10);
+        putToUnControlledMetricsMap(DAC, 0, 15);
 
         //Methods metrics set
-        unControlledMetrics.put(CND.name(), new MetricsValidRangeStub(CND.name(), CND.description(), CND.level().level(),
-                false, 0.00, 0.00, 0, 50));
-        unControlledMetrics.put(LND.name(), new MetricsValidRangeStub(LND.name(), LND.description(), LND.level().level(),
-                false, 0.00, 0.00, 0, 4));
-        unControlledMetrics.put(CC.name(), new MetricsValidRangeStub(CC.name(), CC.description(), CC.level().level(),
-                false, 0.00, 0.00, 0, 50));
-        unControlledMetrics.put(NOL.name(), new MetricsValidRangeStub(NOL.name(), NOL.description(), NOL.level().level(),
-                false, 0.00, 0.00, 0, 10));
+        putToUnControlledMetricsMap(CND, 0, 50);
+        putToUnControlledMetricsMap(LND, 0, 4);
+        putToUnControlledMetricsMap(CC, 0, 50);
+        putToUnControlledMetricsMap(NOL, 0, 10);
+    }
+
+    private void putToControlledMetricsMap(MetricType type, long minLongValue, long maxLongValue) {
+        putToMetricsMap(controlledMetrics, type, false, 0.0, 0.0, minLongValue, maxLongValue);
+    }
+
+    private void putToControlledMetricsMap(MetricType type, double minDoubleValue, double maxDoubleValue) {
+        putToMetricsMap(controlledMetrics, type, true, minDoubleValue, maxDoubleValue, 0, 0);
+    }
+
+    private void putToUnControlledMetricsMap(MetricType type, long minLongValue, long maxLongValue) {
+        putToMetricsMap(unControlledMetrics, type, false, 0.0, 0.0, minLongValue, maxLongValue);
+    }
+
+    private void putToUnControlledMetricsMap(MetricType type, double minDoubleValue, double maxDoubleValue) {
+        putToMetricsMap(unControlledMetrics, type, true, minDoubleValue, maxDoubleValue, 0, 0);
+    }
+
+    private void putToMetricsMap(Map<String, MetricsValidRangeStub> map,
+                                 MetricType type, boolean doubleValue,
+                                 double minDoubleValue, double maxDoubleValue,
+                                 long minLongValue, long maxLongValue) {
+        map.put(type.name(), new MetricsValidRangeStub(type.name(), type.description(), type.level().level(),
+                doubleValue, minDoubleValue, maxDoubleValue, minLongValue, maxLongValue));
     }
 
     public boolean isControlValidRanges() {
