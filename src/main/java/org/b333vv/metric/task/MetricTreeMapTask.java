550d3e341871d6dd453ed39d9ed15a7fe44eb901
b333vv <b333vv@gmail.com>
2025-07-01 11:11:56 +0300

    Refactor(3.1.8): Isolate MetricTreeMapTask logic into a dedicated calculator

--- src/main/java/org/b333vv/metric/task/MetricTreeMapTask.java
+++ src/main/java/org/b333vv/metric/task/MetricTreeMapTask.java
@@ -22,12 +22,13 @@ import com.intellij.openapi.progress.Task;
 import org.b333vv.metric.builder.MetricTreeMapModelCalculator;
 import org.b333vv.metric.event.MetricsEventListener;
 import org.b333vv.metric.model.code.JavaCode;
+import org.b333vv.metric.model.code.JavaProject;
 import org.b333vv.metric.service.CacheService;
 import org.b333vv.metric.ui.treemap.presentation.MetricTreeMap;
 import org.jetbrains.annotations.NotNull;
 
 public class MetricTreeMapTask extends Task.Backgroundable {
-    private static final String GET_FROM_CACHE_MESSAGE = "Try to getProfiles treemap with metric types distribution from cache";
+    private static final String GET_FROM_CACHE_MESSAGE = "Try to get treemap with metric types distribution from cache";
     private static final String STARTED_MESSAGE = "Building treemap with metric types distribution started";
     private static final String FINISHED_MESSAGE = "Building treemap with metric types distribution finished";
     private static final String CANCELED_MESSAGE = "Building treemap with metric types distribution canceled";
@@ -42,8 +43,9 @@ public class MetricTreeMapTask extends Task.Backgroundable {
         MetricTreeMap<JavaCode> metricTreeMap = myProject.getService(CacheService.class).getUserData(CacheService.METRIC_TREE_MAP);
         if (metricTreeMap == null) {
             myProject.getMessageBus().syncPublisher(MetricsEventListener.TOPIC).printInfo(STARTED_MESSAGE);
-            MetricTreeMapModelCalculator calculator = new MetricTreeMapModelCalculator(myProject);
-            metricTreeMap = calculator.calculate(indicator);
+            JavaProject javaProject = myProject.getService(CacheService.class).getProject();
+            MetricTreeMapModelCalculator calculator = new MetricTreeMapModelCalculator();
+            metricTreeMap = calculator.calculate(javaProject);
             myProject.getService(CacheService.class).putUserData(CacheService.METRIC_TREE_MAP, metricTreeMap);
         }
     }
